c547c5e1232926138120f83e15f82d40350f0c3a
Alex Ruco <alex@ruco.pt>
2024-08-09 09:51:31 +0100

    added the url of sitemap on error message

--- dourado/sitemap_validator.py
+++ dourado/sitemap_validator.py
@@ -1,4 +1,3 @@
-# sitemap_validator.py
 import requests
 import xml.etree.ElementTree as ET
 from dourado.utils import log_error, log_success
@@ -18,18 +17,19 @@ def fetch_sitemap(url):
         if response.status_code == 200:
             return response.text
         else:
-            log_error(f"Failed to fetch sitemap: Status code {response.status_code}")
+            log_error(f"Failed to fetch sitemap: Status code {response.status_code} - URL: {url}")
             return None
     except requests.RequestException as e:
-        log_error(f"Failed to fetch sitemap: {e}")
+        log_error(f"Failed to fetch sitemap: {e} - URL: {url}")
         return None
 
-def validate_sitemap(sitemap_content):
+def validate_sitemap(sitemap_content, sitemap_url):
     """
     Validates the sitemap content as XML.
     
     Args:
     sitemap_content (str): The content of the sitemap to validate.
+    sitemap_url (str): The URL of the sitemap being validated.
 
     Returns:
     bool: True if the sitemap is valid XML, False otherwise.
@@ -37,10 +37,10 @@ def validate_sitemap(sitemap_content):
     try:
         # Attempt to parse the sitemap content
         ET.fromstring(sitemap_content)
-        log_success("Sitemap is valid XML.")
+        log_success(f"Sitemap is valid XML - URL: {sitemap_url}")
         return True
     except ET.ParseError as e:
-        log_error(f"Sitemap is invalid XML: {e}")
+        log_error(f"Sitemap is invalid XML: {e} - URL: {sitemap_url}")
         return False
 
 # Example usage
@@ -48,8 +48,8 @@ if __name__ == "__main__":
     sitemap_url = 'https://mysitefaster.com/sitemap_index.xml'
     sitemap_content = fetch_sitemap(sitemap_url)
     if sitemap_content:
-        log_success(f"Fetched sitemap content:\n{sitemap_content[:200]}...")  # Log only the first 200 characters
-        is_valid = validate_sitemap(sitemap_content)
+        log_success(f"Fetched sitemap content from {sitemap_url}:\n{sitemap_content[:200]}...")  # Log only the first 200 characters
+        is_valid = validate_sitemap(sitemap_content, sitemap_url)
         if is_valid:
             log_success("Sitemap validation passed.")
         else:
